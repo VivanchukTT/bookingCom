e544f007868b1b060914c40363874fd4dcf2669c
VivanchukTT <VivanchukTT@users.noreply.github.com>
2019-10-04 12:19:39 +0300

    Add project files.

---
+++ TestQA/Base.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+namespace TestQA
+{
+    public class Base
+    {
+        protected IWebDriver driver;
+        public void Setup(String browserName)
+        {
+            if (browserName.Equals("ie"))
+            {
+                driver = new InternetExplorerDriver();
+            }
+            else if (browserName.Equals("chrome"))
+            {
+                driver = new ChromeDriver();
+            }
+            else if (browserName.Equals("firefox"))
+            {
+                driver = new FirefoxDriver();
+            }
+        }
+
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+        public static IEnumerable<String> BrowserToRun()
+        {
+            String[] browsers = { "chrome", "firefox"};
+            foreach (String b in browsers)
+            {
+                yield return b;
+            }
+        }
+    }
+}
---
+++ TestQA/BookingTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using System.Threading.Tasks;
+
+
+namespace TestQA
+{
+    [TestFixture]
+    public class BookingTest : Base
+    {  
+        [Test]
+        [Parallelizable]
+        [TestCaseSource(typeof(Base), "BrowserToRun")]
+        public void BookingComTest(String browserName)
+        {
+            Setup(browserName);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            /* 1. should go to the https://www.booking.com page */
+
+            driver.Url = "https://www.booking.com/index.en-gb.html";
+
+            /* 2. Fill in the form with values:Destination: “New York” Check-in: <current  date> Check-out: <current date + 7 days> And submit the search form. */
+
+            driver.FindElement(By.Id("ss")).SendKeys("New York");
+            driver.FindElement(By.XPath("//*[@id=\"frm\"]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]")).Click();
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            driver.FindElement(By.XPath("//*[contains(@data-date,'"+ today + "')]")).Click();
+            DateTime newDate = DateTime.Today.AddDays(7);
+            driver.FindElement(By.XPath("//*[contains(@data-date,'" + newDate.ToString("yyyy-MM-dd") + "')]")).Click();
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+            Task.Delay(1500).Wait();
+
+            /* 3.On the search results page it should check whether there is at least 1 result 
+             and all results are from New York(it should contain “New York” in the location text) */
+
+            var results = driver.FindElement(By.ClassName("bui-link"));
+            string city = "New York";
+            try
+            {
+                Assert.IsTrue(results.Displayed);
+            }
+            catch (AssertionException)
+            {
+                Console.WriteLine("Assert failed, no results returned");
+            }
+            try
+            {
+                Assert.IsTrue(results.Text.Contains(city));
+            }
+            catch (AssertionException)
+            {
+                Console.WriteLine("Assert failed, no required specific results returned");               
+            }
+            
+        }
+    }
+}
