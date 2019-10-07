62bea9ef6c6899dafa503de3676b998f4732fe8c
VivanchukTT <VivanchukTT@users.noreply.github.com>
2019-10-07 17:41:08 +0300

    Update BookingTest.cs

--- TestQA/BookingTest.cs
+++ TestQA/BookingTest.cs
@@ -24,18 +24,17 @@ namespace TestQA
             /* 2. Fill in the form with values:Destination: “New York” Check-in: <current  date> Check-out: <current date + 7 days> And submit the search form. */
 
             driver.FindElement(By.Id("ss")).SendKeys("New York");
-            driver.FindElement(By.XPath("//*[@id=\"frm\"]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]")).Click();
+            driver.FindElement(By.XPath("//*[@id='frm']/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]")).Click();
             string today = DateTime.Now.ToString("yyyy-MM-dd");
             driver.FindElement(By.XPath("//*[contains(@data-date,'"+ today + "')]")).Click();
             DateTime newDate = DateTime.Today.AddDays(7);
             driver.FindElement(By.XPath("//*[contains(@data-date,'" + newDate.ToString("yyyy-MM-dd") + "')]")).Click();
             driver.FindElement(By.XPath("//button[@type='submit']")).Click();
-            Task.Delay(1500).Wait();
 
             /* 3.On the search results page it should check whether there is at least 1 result 
              and all results are from New York(it should contain “New York” in the location text) */
 
-            var results = driver.FindElement(By.ClassName("bui-link"));
+            var results = driver.FindElement(By.XPath("//*[@id='hotellist_inner']//*[@class='bui-link']"));
             string city = "New York";
             try
             {
